5449914b93bee1d0d22141d2e7a297f8b5cb0534
Ratoriku <a1063021545@gmail.com>
2022-06-01 23:39:23 +0800

    overlay: Import Oppo Sans font
    * Extracted from op8 oos12 dump
    * Font weight config value from njzy
    
    Change-Id: I6144e3efb0a478000b1d63089e1723dcd16d9ca6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     FontHarmonySansOverlay \
     FontInterOverlay \
     FontManropeOverlay \
+    FontOppoSansOverlay \
     FontRobotoOverlay \
     FontUrbanistOverlay \
     IconPackCircularAndroidOverlay \
